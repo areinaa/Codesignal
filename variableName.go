1056850cae2d54fc66b5aa5e053721389065ea00
Areina <84774595+areinaa@users.noreply.github.com>
2024-01-07 13:20:22 +0100

    Arcade Intro 32

---
+++ variableName.go
@@ -0,0 +1,16 @@
+func solution(name string) bool {
+    
+    if (name[0]>=48 && name[0]<= 57){
+        return false
+    }
+    
+    for _, val := range name {
+        if (val == 95 || (val >= 97 && val <=122) || (val >= 65 && val <=90) || (val>=48 && val<= 57)){
+            continue
+        }else{
+            return false
+        }
+    }
+    
+    return true
+}
